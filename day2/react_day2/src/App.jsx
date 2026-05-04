37a18b06f625a1af3f564569e6f2d4e774b48ae2
Sufal K.c. <kcsufal1@gmail.com>
2026-05-04 11:38:56 +0545

    Day 2 updated

---
+++ day2/react_day2/src/App.jsx
@@ -0,0 +1,15 @@
+import { Greetings } from "./Greetings";
+
+
+function App() {
+  return (
+    <>
+      <Greetings/>
+      <Greetings/>
+      <Greetings/>
+      <Greetings/>
+    </>
+  )
+}
+
+export default App
