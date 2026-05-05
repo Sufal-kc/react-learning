8133947f3bc504365465f623a4d5612b5c0232b7
Sufal K.c. <kcsufal1@gmail.com>
2026-05-05 06:23:36 +0545

    code correcte3

--- day2_3/react_day2/src/Greetings.jsx
+++ day2_3/react_day2/src/Greetings.jsx
@@ -1,6 +1,8 @@
-export const ({name="Guest", message="Hello"}) => {
+export const Greetings = ({name="Guest", message="Hello"}) => {
 return(
     <h2>{message}, {name}! </h2>
 );
-}
-
+};
+/*the null and the default value are different.
+the default value is used when when the prop is missng
+whereas null valuei us used when the prop is passed but with null value. ->that how js works*/
